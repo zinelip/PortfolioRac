11a817e18db1203d3583af732516a0ed971787b7
zinelip <zinelip@gmail.com>
2024-06-04 11:05:30 +0200

    first commit

---
+++ W proyect/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <el-row>
+      <el-col :span="24">
+        <div class="grid-content bg-purple-dark"></div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script setup>
+// No necesitas importar nada aquí si no hay lógica
+</script>
+
+<style scoped>
+/* tu CSS aquí */
+</style>
